8629ce3b9ccfddb6f702da6c36a94fe647d6acc8
Student <student@c006.local>
2018-11-20 15:38:28 +0300

    Initial Commit

---
+++ Apple pie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple pie
+//
+//  Created by Student on 20/11/2018.
+//  Copyright © 2018 Dangus66. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
